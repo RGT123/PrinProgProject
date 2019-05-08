fc4b75b0b14d428b5f04ca9495729d1ab71174a7
saikoka <46795444+saikoka@users.noreply.github.com>
2019-05-08 12:59:54 -0400

    Dog Class

---
+++ Dog.py
@@ -0,0 +1,18 @@
+class doggie:
+    def __init__(self, points=0, time, alone, exerciseTime, allergies, salary, children, personality, size, travellingTime, dentistTime, temperature, livingArrangements, dogClothing, tenYears):
+        self.time=time
+        self.alone=alone
+        self.exerciseTime=exerciseTime
+        self.allergies=allergies
+        self.salary=salary
+        self.children=children
+        self.personality=personality
+        self.size=size
+        self.travellingTime=travellingTime
+        self.dentistTime=dentistTime
+        self.temperature=temperature
+        self.livingArrangements=livingArrangements
+        self.dogClothing=dogClothing
+        self.tenYears=tenYears
+
+
