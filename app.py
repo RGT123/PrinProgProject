cdd1de88f5f6f68a96df19c8564c9fe9a38fa909
RGT123 <ruben.Thomas.nj@gmail.com>
2019-05-11 21:20:06 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import Driver
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 
 
@@ -7,8 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def getDogs(freeTime,alone,exercise,allergies,salary,children,reaction,fatSkinny,patience,temperature,grooming):
+    print (freeTime + alone+exercise+allergies+salary+children+reaction+fatSkinny+patience+temperature+grooming)
+
+
 
 @app.route('/', methods=['POST'])
+
 def getvalue():
     freeTime = request.form['freeTime']
     alone = request.form['alone?']
@@ -21,11 +26,23 @@ def getvalue():
     patience=request.form['patience']
     temperature=request.form['temperature']
     grooming=request.form['grooming']
-    print(fatSkinny)
-    return freeTime
-
+    #getDogs(freeTime,alone,exercise,allergies,salary,children,reaction,fatSkinny,patience,temperature,grooming)
+    Driver.getFreeTime(freeTime)
+    Driver.getAlone(alone)
+    Driver.getExercise(exercise)
+    Driver.getAllergies(allergies)
+    Driver.getSalary(salary)
+    Driver.getChildren(children)
+    Driver.getReaction(reaction)
+    Driver.getFatSkinny(fatSkinny)
+    Driver.getPatience(patience)
+    Driver.getTemperature(temperature)
+    Driver.getGrooming(grooming)
+    Driver.main()
+    return grooming
 
 
 
 if __name__ == '__main__':
+
     app.run(debug=True)
